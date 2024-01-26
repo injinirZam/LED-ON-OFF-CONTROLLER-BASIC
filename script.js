91177d9199671863d5b58b1790a2d34fb456af77
injinirZam <154145629+injinirZam@users.noreply.github.com>
2024-01-26 12:19:23 +0800

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('onButton').addEventListener('click', function() {
     // Send a request to Arduino to turn the LED on
-    fetch('http://192.168.1.95/on');
+    fetch('https://192.168.1.95/on');
 });
 
 document.getElementById('offButton').addEventListener('click', function() {
     // Send a request to Arduino to turn the LED off
-    fetch('http://192.168.1.95/off');
+    fetch('https://192.168.1.95/off');
 });
